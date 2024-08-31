548b91b2f39e4bb0c5c74d6cccc85420bd0476bd
haree <hareyadav8989@gmail.com>
2024-08-31 15:39:52 +0530

    commit

--- cupperhugo/cupperhugo/settings.py
+++ cupperhugo/cupperhugo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "whitenoise.runserver_nostatic",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -42,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -117,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_DIRS = [os.path.join(BASE_DIR,'copper/static')]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
---
+++ cupperhugo/staticfiles/copper/js/nav.js
@@ -0,0 +1,10 @@
+const element=document.getElementById("hoverefect");
+
+element.addEventListener( 'mouseenter', function() {
+    element.style.backgroundColor='red';
+
+});
+
+// element.addEventListener('mouseout', function(){
+//     element.style.backgroundColor='lightblue';
+// })
